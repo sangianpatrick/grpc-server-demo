f85b96ecd131780ee247feb2c88347e5efa87e5c
Patrick Maurits Sangian <patricksangian@gmail.com>
2020-07-30 12:17:31 +0700

    remake architecture

--- protogen.sh
+++ protogen.sh
@@ -1,1 +1,3 @@
-protoc --proto_path=$GOPATH/src/github.com/google/protobuf/src --proto_path=./proto --go_out=plugins=grpc:./pb ./proto/*.proto
+#protoc --proto_path=$GOPATH/src/github.com/google/protobuf/src --proto_path=./proto --go_out=plugins=grpc:./src/pb ./proto/*.proto
+
+protoc --proto_path=./proto --go_out=plugins=grpc:./src/pb ./proto/*.proto
